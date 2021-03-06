82bc3d827bf9cd3fcbfc2423c9b3a23f12d45464
Lilan Manoj <lilan.emedia@gmail.com>
2021-03-07 02:53:16 +0530

    Added & config tntsearch scout driver

--- config/scout.php
+++ config/scout.php
@@ -116,4 +116,28 @@ return [
         'secret' => env('ALGOLIA_SECRET', ''),
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | tntsearch
+    |--------------------------------------------------------------------------
+    | A fully featured full text search engine written in PHP
+    | https://opencollective.com/tntsearch
+    | tntsearch driver for Laravel Scout can be found on:
+    | https://github.com/teamtnt/laravel-scout-tntsearch-driver
+    |
+    */
+
+    'tntsearch' => [
+        'storage'  => storage_path(), //place where the index files will be stored
+        'fuzziness' => env('TNTSEARCH_FUZZINESS', false),
+        'fuzzy' => [
+            'prefix_length' => 2,
+            'max_expansions' => 50,
+            'distance' => 2
+        ],
+        'asYouType' => false,
+        'searchBoolean' => env('TNTSEARCH_BOOLEAN', false),
+        'maxDocs' => env('TNTSEARCH_MAX_DOCS', 500),
+    ],
+
 ];
