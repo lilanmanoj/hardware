14d38aa6e8af20b8812fcd3362c7fcfa310a1846
Lilan Manoj <lilan.emedia@gmail.com>
2021-03-17 02:18:52 +0530

    Landing page: header section

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,32 +1,89 @@
 <template>
-    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-        <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            <inertia-link v-if="$page.props.user" href="/dashboard" class="text-sm text-gray-700 underline">
-                Dashboard
-            </inertia-link>
+    <div class="bg-theme-light relative overflow-hidden m-0 p-0 w-full">
+        <div id="header" class="overflow-hidden w-full relative">
+            <div class="my-4 mx-auto w-4/5 h-full relative">
+                <nav class="inline-flex text-theme-light font-bold text-xl">
+                    <img alt="Logo" src="../../assets/images/logo.png" class="logo">
 
-            <template v-else>
-                <inertia-link :href="route('login')" class="text-sm text-gray-700 underline">
-                    Log in
-                </inertia-link>
+                    <li><a href="#about">About Us</a></li>
+                    <li><a href="#">Explore</a></li>
+                    <li><a href="#">Contact Us</a></li>
 
-                <inertia-link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 underline">
-                    Register
-                </inertia-link>
-            </template>
+                    <li>
+                        <input type="text" placeholder="Search" class="search bg-transparent flex-grow border-gray-300 rounded-full pl-6">
+                    </li>
+
+                    <div class="absolute right-0 mt-5 text-lg" v-if="$page.props.user">
+                        <inertia-link href="/dashboard" class="bg-transparent border border-theme-yellow rounded-lg font-bold text-theme-yellow py-2 px-4">
+                            Dashboard
+                        </inertia-link>
+                    </div>
+
+                    <template v-else>
+                        <div class="absolute right-0 mt-5 text-lg">
+                            <inertia-link v-if="canRegister" :href="route('register')" class="bg-transparent border border-theme-yellow rounded-lg font-bold text-theme-yellow py-2 px-4">
+                                Sign Up
+                            </inertia-link>
+
+                            <inertia-link :href="route('login')" class="bg-theme-dark border border-theme-yellow rounded-lg font-bold text-theme-yellow max-h-10 py-2 px-5 ml-5">
+                                Sign In
+                            </inertia-link>
+                        </div>
+                    </template>
+                </nav>
+                
+                <div class="transform translate-y-1/2">
+                    <h1 class="text-7xl text-theme-light font-extrabold w-1/2">Lorem ipsum dolor</h1>
+                    <h1 class="text-6xl text-theme-yellow font-extrabold w-2/5">sit amet, eos laudem</h1>
+                    <p class="text-base text-theme-light font-bold text-opacity-80 w-1/2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed bibendum diam in leo lacinia gravida. 
+                    Etiam luctus velit non odio egestas, eu luctus massa tempus.</p>
+
+                    <form class="bg-theme-light w-1/2 mt-12 p-6 rounded-lg">
+                        <h4 class="text-normal font-bold">Join  Now Today!</h4>
+
+                        <div class="flex gap-x-4 mt-3">
+                            <input type="text" placeholder="Enter your email address here" class="flex-grow border-gray-300 rounded-full pl-6">
+                            <button type="submit" class="bg-theme-yellow px-5 rounded-lg font-bold">Join Now</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
 
-        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-            <h1 class="text-center text-4xl md:text-7xl font-semibold text-blue-700">{{ appName }}</h1>
-            <h3 class="text-center mt-3 grid-cols-2">
-                <svg class="animate-ping bg-green-500 h-3 w-3 rounded-full mr-4 self-center inline"></svg>
-                <span class="w-auto text-lg font-semibold">Online</span>
-            </h3>
+        <div id="about" class="overflow-hidden w-full relative">
+            <div class="my-4 mx-auto w-4/5 h-full relative">
+
+            </div>
         </div>
     </div>
 </template>
 
 <style scoped>
+    #header {
+        background-image: linear-gradient(to bottom, #000000c2, #0000005f), url('../../assets/images/header.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        height: 90vh;
+    }
+
+    #header nav {
+        list-style: none;
+    }
+
+    #header nav li {
+        margin: 14px 10px 0 24px;
+    }
+
+    #header nav .search {
+        width: 300px;
+    }
+
+    #header nav .logo {
+        width: 120px;
+        margin-right: 20px;
+    }
+
     .bg-gray-100 {
         background-color: #f7fafc;
         background-color: rgba(247, 250, 252, var(--tw-bg-opacity));
@@ -51,6 +108,11 @@
             canLogin: Boolean,
             canRegister: Boolean,
             appName: String,
+        },
+        methods: {
+            handleSelect(key, keyPath) {
+                console.log(key, keyPath);
+            }
         }
     }
 </script>
