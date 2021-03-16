de3227ba181c7daf860cf1a0e8bd3a8f56fe4169
Lilan Manoj <lilan.emedia@gmail.com>
2021-03-17 02:17:16 +0530

    Added theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
     purge: [
@@ -13,6 +14,11 @@ module.exports = {
         extend: {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                'theme-light': '#fff',
+                'theme-dark': '#1A1A1A',
+                'theme-yellow': '#FCCC20',
             },
         },
     },
