cd9c481c2e524ccca9d4a9be4cda9f1129fc4b51
Lilan Manoj <lilan.emedia@gmail.com>
2021-05-05 23:22:55 +0530

    Added district and area entities with relations

---
+++ database/migrations/2021_05_05_173349_create_districts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDistrictsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('districts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->nullable(false);
+            $table->double('latitude')->nullable(true)->comment('Latitude of the center of district');
+            $table->double('longitude')->nullable(true)->comment('Longitude of the center of district');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('districts');
+    }
+}
