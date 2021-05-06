d456f7601d39ce8b1292fbdcd6121488b0f483c9
Lilan Manoj <lilan.emedia@gmail.com>
2021-05-07 00:23:13 +0530

    Sass configured

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,6 +17,7 @@ mix.js('resources/js/app.js', 'public/js').vue()
         require('tailwindcss'),
         require('autoprefixer'),
     ])
+    .sass('resources/sass/app.scss', 'public/css')
     .webpackConfig(require('./webpack.config'));
 
 if (mix.inProduction()) {
